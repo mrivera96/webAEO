3797aab9aabb688f64f5cdd131a47fcaae14047b
mrivera96 <jylrivera96@gmail.com>
2018-07-21 16:51:32 -0600

    Añadir validaciones y cuadros de diálogo en nuevo perfil

--- js/nuevoPerfil.js
+++ js/nuevoPerfil.js
@@ -1,4 +1,3 @@
-
 $(document).on("ready", function(){ loadData(); });
 
 var loadData = function(){
@@ -38,6 +37,33 @@ var loadData = function(){
 
 document.getElementById("guardar").onclick=function(){validarFormulario();};
 
+function mostrarError(componente, error){
+        
+         $("#formularioCrear").append('<div class="modal" id="Modal3" tabindex="-1" role="dialog">'+
+                            '<div class="modal-dialog" role="document">'+
+                                '<div class="modal-content">'+
+                                    '<div class="modal-header">'+
+                                        '<h5 class="modal-title">Error al crear el perfil</h5>'+
+                                        '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
+                                            '<span aria-hidden="true">&times;</span>'+
+                                        '</button>'+
+                                    '</div>'+
+                                   ' <div class="modal-body">'+
+                                        '<p>'+error+'</p>'+
+                                    '</div>'+
+                                    '<div class="modal-footer">'+
+                                        '<button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>');
+                $("#Modal3").modal("show");
+                $('#Modal3').on('hidden.bs.modal', function () {
+                    componente.focus();   
+                    $("#Modal3").detach();
+                });
+                
+    }
 function validarFormulario(){
         var error_nomb=false;
         var error_tel=false; 
@@ -49,10 +75,9 @@ function validarFormulario(){
         var error_cat=false;
         
         
-        if(document.formularioCrear.nomborg_rec.value===""){
-            error_nomb=true;
-            alert('Debe ingresar un nombre de organización.');
-            document.formularioCrear.nomborg_rec.focus();
+         if(document.formularioCrear.nomborg_rec.value===""){
+            error_nomb=true;                  
+            mostrarError(document.formularioCrear.nomborg_rec,"Debe ingresar un nombre de organización.");
             return;
         }
         
@@ -61,8 +86,8 @@ function validarFormulario(){
         }else{
             if(!document.formularioCrear.email_rec.value.includes("@") || !document.formularioCrear.email_rec.value.includes(".")){
                 error_mail=true;
-                alert('Ingrese un e-mail válido.');
-                document.formularioCrear.email_rec.focus();
+                
+                mostrarError(document.formularioCrear.email_rec,"Ingrese un e-mail válido.");
                 return;
             }
         }
@@ -71,15 +96,15 @@ function validarFormulario(){
         if(document.formularioCrear.numtel_rec.value===""){
             if(document.formularioCrear.numcel_rec.value===""){
                 error_tel=true;
-                alert('Debe ingresar al menos un número telefónico.');
-                document.formularioCrear.numtel_rec.focus();
+                
+                mostrarError(document.formularioCrear.numtel_rec,"Debe ingresar al menos un número telefónico.");
                 return;
             }
         }else{
             if(document.formularioCrear.numtel_rec.value.length < 8 || !document.formularioCrear.numtel_rec.value.startsWith("2") || document.formularioCrear.numtel_rec.value.length>8){
                 error_tel=true;
-                alert('El número telefónico ingresado no es válido.');
-                document.formularioCrear.numtel_rec.focus();
+                
+                mostrarError(document.formularioCrear.numtel_rec,"El número telefónico ingresado no es válido.");
                 return;
             }
         }
@@ -87,35 +112,35 @@ function validarFormulario(){
         if(document.formularioCrear.numcel_rec.value===""){
             if(document.formularioCrear.numtel_rec.value===""){
                 error_cel=true;
-                alert('Debe ingresar al menos un número telefónico.');
-                document.formularioCrear.numcel_rec.focus();
+               
+                mostrarError(document.formularioCrear.numcel_rec,"Debe ingresar al menos un número telefónico.");
                 return;
             }
         }else{
             if(document.formularioCrear.numcel_rec.value.length<8 || document.formularioCrear.numcel_rec.value.length>8){
                 error_cel=true;
-                alert('El número telefónico ingresado no es válido.');
-                document.formularioCrear.numcel_rec.focus();
+                $("#Modal3").modal("show");
+                mostrarError(document.formularioCrear.numcel_rec,"El número telefónico ingresado no es válido.");
                 return;
             }
         }
 
         if(document.formularioCrear.direccion_rec.value===""){
             error_dir=true;
-            alert('Debe ingresar la dirección de la organización.');
-            document.formularioEditar.direccion_rec.focus();
+          
+            mostrarError(document.formularioCrear.direccion_rec,"Debe ingresar la dirección de la organización.");
             return;
         }
         if(document.formularioCrear.desc_rec.value===""){
             error_desc=true;
-            alert('Debe escribir una descripción de la organización.');
-            document.formularioCrear.desc_rec.focus();
+           
+            mostrarError(document.formularioCrear.desc_rec,"Debe escribir una descripción de la organización.");
             return;
         }
         if(document.formularioCrear.id_region.value < 3 && document.formularioCrear.id_region.value > 4){
             error_reg=true;
             alert('La región seleccionada no existe.');
-            document.formularioCrear.id_region.focus();
+            document.formularioEditar.id_region.focus();
             return;
         }
         if(document.formularioCrear.id_categoria.value < 1 && document.formularioCrear.id_categoria.value > 11 ){
@@ -134,8 +159,8 @@ function validarFormulario(){
            error_reg    ===false &&
            error_cat    ===false){
                 document.formularioCrear.submit();
+                $("#Modal1").modal('show');
                 
-                window.location.href = 'administracion-de-perfiles.php';
                 return;
         }
     }
