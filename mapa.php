d4f1192380a7070af3c8efe0d6071ef91bc996a2
bessyRamos <bessyramos3@gmail.com>
2018-08-05 11:04:52 -0600

    modificacion de algunos resultados del mapa y de resultado de busqueda

--- mapa.php
+++ mapa.php
@@ -7,11 +7,11 @@ include_once 'plantillas/barra-de-navegacion-navbar.inc.php';
 <script src="js/jquery-2.2.4.min.js"></script>
 
 <div id="encabezado_resultado" class="container responsive">
-    <br><h4 class="text-center" id="nombreCategoria">Ubicación</h4>
+    <h4 class="text-center" id="nombreCategoria">Ubicación</h4>
 </div>
 <br/>
 <div class="container responsive" id="contenedor_resultado"> 
-
+    <br>
     <script type="text/javascript" 
             src="https://maps.google.com/maps/api/js?sensor=false">
     </script> 
@@ -34,28 +34,32 @@ include_once 'plantillas/barra-de-navegacion-navbar.inc.php';
                         var coordenadas = JSON.parse(data);
                         var latitud;
                         var longitud;
-
+                     
 
                         for (var i in coordenadas)
                         {
+                          
+                            
                             $("#titulo").append(
                                     '<h3' + coordenadas[i].nombre_organizacion + '</h3>'
+                                  
 
                                     );
 
-                            if (coordenadas[i].latitud & coordenadas[i].longitud != "") {
+                            if (coordenadas[i].latitud && coordenadas[i].longitud != "") {
                                 var myLatlng = new google.maps.LatLng(latitud = coordenadas[i].latitud, longitud = coordenadas[i].longitud);
 
                                 var myOptions = {
                                     zoom: 15,
                                     center: myLatlng,
-                                    mapTypeId: google.maps.MapTypeId.satelite,
+                                    mapTypeId:google.maps.MapTypeId.ROADMAP,
                                 }
                                 var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
                                 marker = new google.maps.Marker({
                                     position: myLatlng,
                                     title: coordenadas[i].nombre_organizacion,
+                                   
                                 });
                                 google.maps.event.addListener(marker, "dragend", function () {
 
@@ -83,7 +87,7 @@ include_once 'plantillas/barra-de-navegacion-navbar.inc.php';
                 }
     </script>	
     <body >
-        <div id="map_canvas" style="width:100%; height:400px"></div><br> 
+        <div id="map_canvas" style="width:100%; height:435px"></div><br> 
 
     </body>
 
