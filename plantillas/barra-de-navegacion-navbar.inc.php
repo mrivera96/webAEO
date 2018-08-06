fd009e1118957228cbdb17e719e2db7059c6efa3
SheyliSanchez <vanessa1994sanchez@hotmail.com>
2018-08-06 12:20:50 -0600

    funcionamiento de editar un usuario cliente

--- plantillas/barra-de-navegacion-navbar.inc.php
+++ plantillas/barra-de-navegacion-navbar.inc.php
@@ -24,7 +24,12 @@
                         <!--   header('Location: /webaeo/contactosUsuario.php');
                         } else if (($_SESSION['normal'] == 1) && ($_SESSION['actividad'] == 1)) {
                             header('Location: /webaeo/mostrar_usuarios.php'); -->
-                          <li> <a id = "colorIniciosecion" href = "login.php"><span class = "glyphicon glyphicon-cog" aria-hidden = "true"></span> <strong>Panel de Control </strong></a></li>
+                           <li id="boton" class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <strong>Panel de Control</strong><span class="caret"></span></a>
+                            <ul id="despliege"class="dropdown-menu" role="menu">
+                                <li><a href="editarUsuarioNormal.php"><img src="imagenes/administracioncuenta.jpg" height="15"></img> <strong>Edición de Cuenta</strong></a></li>
+                            </ul>
+                        </li>
                           <li> <a id="colorIniciosecion" href="cerrarSessionLogin.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> <strong>Cerrar Sesión</strong></a></li>
                           
                         <?php
