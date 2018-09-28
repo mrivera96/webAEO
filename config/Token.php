bd76f6f8e7ad90f859dde75129845468fdc688fe
mrivera96 <jylrivera96@gmail.com>
2018-09-28 17:53:26 -0600

    cambios en metodo generarToken

--- config/Token.php
+++ config/Token.php
@@ -2,7 +2,7 @@
 require_once '../php-jwt-master/src/JWT.php';
 require_once '../php-jwt-master/src/BeforeValidException.php';
 require_once '../php-jwt-master/src/ExpiredException.php';
-require_once '../hp-jwt-master/src/SignatureInvalidException.php';
+require_once '../php-jwt-master/src/SignatureInvalidException.php';
 
 
 use Firebase\JWT\JWT;
