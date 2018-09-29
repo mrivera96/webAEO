847ea6afad779b04d8a40d951e907ca5c16a8a31
bessyRamos <bessyramos3@gmail.com>
2018-09-29 13:32:49 -0600

    existencia y vigencia de token.

--- config/cerrarSessionLogin.php
+++ config/cerrarSessionLogin.php
@@ -1,7 +1,12 @@
 <?php
+include_once 'Token.php';
+
   session_start();
+  Token::revocarToken($_SESSION['token']);
   session_unset();
+  
   session_destroy();
+  
   header('Location: /webaeo');
 ?>
 
