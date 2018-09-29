d422135721b24a0e11453bf9ae2712488ab5ef13
bessyRamos <bessyramos3@gmail.com>
2018-09-29 16:51:46 -0600

    colocacion del token eliminarPerfil

--- WebServices/eliminarPerfil.php
+++ WebServices/eliminarPerfil.php
@@ -1,20 +1,31 @@
 <?php
  include '../config/ConexionABaseDeDatos.php';
  include_once '../config/Errores.inc.php';
+ include_once '../config/Token.php';
 
 
- if(isset($_GET["cto"])){
- $id_contacto=$_GET['cto'];
- 
-  
-$update="update contactos SET id_estado=4 where id_contacto=?";
-$resultado_update=$con->prepare($update);
-$resultado_update->bind_param("i",$id_contacto);
-$resultado_update->execute();
- }else{
+ if(isset($_POST["cto"])&&isset($_POST['tkn']) && !empty($_POST['tkn'])){
+     
+        if( Token::existeToken($_POST['tkn']) ){
+            if(Token::vigenciaToken($_POST['tkn'])){
+                $id_contacto=$_POST['cto'];
+
+
+               $update="update contactos SET id_estado=4 where id_contacto=?";
+               $resultado_update=$con->prepare($update);
+               $resultado_update->bind_param("i",$id_contacto);
+               $resultado_update->execute();
+               
+            }else{
+                print json_encode("El Token ya expiró.");
+            }
+        }else{
+            print json_encode("El token recibido NO existe en la base de datos.");
+        }
+    }else{
      print json_encode(ERROR22);
- }
- 
+      }
+
 $con->close();
  
 ?> 
