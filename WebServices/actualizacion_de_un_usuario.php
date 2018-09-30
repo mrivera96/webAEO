13c182e7d26244c824956e54323640ef4379e743
bessyRamos <bessyramos3@gmail.com>
2018-09-30 14:16:10 -0600

    agregado del token en actualizacion de un usuario.

--- WebServices/actualizacion_de_un_usuario.php
+++ WebServices/actualizacion_de_un_usuario.php
@@ -2,63 +2,71 @@
 
 include '../config/ConexionABaseDeDatos.php';
 include '../config/Errores.inc.php';
-$flag = array();
-$error_nomUsuario = false;
-$error_nomPropio = false;
-$error_correo = false;
+include_once '../config/Token.php';
+if(isset($_POST['tkn']) && !empty($_POST['tkn'])){
+  if( Token::existeToken($_POST['tkn']) ){
+    if(Token::vigenciaToken($_POST['tkn'])){
+        $flag = array();
+        $error_nomUsuario = false;
+        $error_nomPropio = false;
+        $error_correo = false;
 
 
-if (!isset($_POST['usuarionombre']) || empty($_POST['usuarionombre'])) {
-    $error_nomUsuario = true;
-    print json_encode(ERROR10);
-    return;
-}
-if (!isset($_POST['usuariopropio']) || empty($_POST['usuariopropio'])) {
-    $error_nomPropio = true;
-    print json_encode(ERROR11);
-    return;
-}
+        if (!isset($_POST['usuarionombre']) || empty($_POST['usuarionombre'])) {
+            $error_nomUsuario = true;
+            print json_encode(ERROR10);
+            return;
+        }
+        if (!isset($_POST['usuariopropio']) || empty($_POST['usuariopropio'])) {
+            $error_nomPropio = true;
+            print json_encode(ERROR11);
+            return;
+        }
 
-if (!isset($_POST['usuarioemail']) || empty($_POST['usuarioemail'])) {
-    $error_correo = true;
-    print json_encode(ERROR32);
-    return;
-} else {
-    if (strpos($_POST['usuarioemail'], "@") === false || strpos($_POST['usuarioemail'], ".") === false) {
-        $error_correo = true;
-        print json_encode(ERROR4);
-        return;
+        if (!isset($_POST['usuarioemail']) || empty($_POST['usuarioemail'])) {
+            $error_correo = true;
+            print json_encode(ERROR32);
+            return;
+        } else {
+            if (strpos($_POST['usuarioemail'], "@") === false || strpos($_POST['usuarioemail'], ".") === false) {
+                $error_correo = true;
+                print json_encode(ERROR4);
+                return;
+            }
+        }
+
+
+
+        if ($error_nomUsuario === false &&
+                $error_nomPropio === false &&
+                $error_correo === false
+
+        ) {
+
+        //if (isset($_POST["id_usuario"]) && isset($_POST["nombre_usuario"]) && isset($_POST["nombre_propio"]) && isset($_POST["correo"]) ) {
+
+            $id_usua = $_POST['usuario'];
+            $nombre_usua = $_POST['usuarionombre'];
+            $nombre_pro = $_POST['usuariopropio'];
+            $corr = $_POST['usuarioemail'];
+
+
+
+            $query_search = "update usuarios SET nombre_usuario=?,nombre_propio=?,correo=? where id_usuario=?";
+
+            $resultado = $con->prepare($query_search);
+            $resultado->bind_param("sssi", $nombre_usua, $nombre_pro, $corr,  $id_usua);
+            $resultado->execute();
+         print json_encode(ERROR13);
+        }
+      } else{
+          print json_encode("El Token ya expiró.");
+        }
+  }else{
+        print json_encode("El token recibido NO existe en la base de datos.");
     }
-}
-
-
-
-if ($error_nomUsuario === false &&
-        $error_nomPropio === false &&
-        $error_correo === false 
-      
-) {
-
-//if (isset($_POST["id_usuario"]) && isset($_POST["nombre_usuario"]) && isset($_POST["nombre_propio"]) && isset($_POST["correo"]) ) {
-
-    $id_usua = $_POST['usuario'];
-    $nombre_usua = $_POST['usuarionombre'];
-    $nombre_pro = $_POST['usuariopropio'];
-    $corr = $_POST['usuarioemail'];
-
-    
-
-    $query_search = "update usuarios SET nombre_usuario=?,nombre_propio=?,correo=? where id_usuario=?";
-
-    $resultado = $con->prepare($query_search);
-    $resultado->bind_param("sssi", $nombre_usua, $nombre_pro, $corr,  $id_usua);
-    $resultado->execute();
- print json_encode(ERROR13);
-} else {
+}else {
     print json_encode(ERROR22);
 }
-
-
-
 $con->close();
 ?>
